1d9aa05174186fcc0908886c1e8530a850d7a7f4
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2024-05-19 17:43:05 +0700

    Create Creating Permanent Tables and Access-Controlled Views in BigQuery_8.sql

---
+++ Creating Permanent Tables and Access-Controlled Views in BigQuery_8.sql
@@ -0,0 +1,12 @@
+#standardSQL
+SELECT DISTINCT
+  date,
+  fullVisitorId,
+  CAST(visitId AS STRING) AS visitId,
+  channelGrouping,
+  totalTransactionRevenue / 1000000 AS totalTransactionRevenue
+ FROM `data-to-insights.ecommerce.all_sessions_raw`
+ WHERE totalTransactionRevenue IS NOT NULL
+ ORDER BY date DESC # latest transactions
+ LIMIT 100
+;
